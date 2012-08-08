000af2b2441567213e2e239a845e11accec131ff
Julien Tanguy <julien.tanguy@jhome.fr>
2012-08-08 14:35:15 +0200

    Add dot export

--- Data/Automata/DFA.hs
+++ Data/Automata/DFA.hs
@@ -1,13 +1,22 @@
-
+{-# LANGUAGE OverloadedStrings #-}
+-- |The module Data.Automata.DFA provides Deterministic Finite Automata
+-- type and methods to create, walk, test whether it accepts a word,
+-- and check for completeness
+--
+-- *TODO
+-- - Validate a DFA, by checking if the set of finite states is a subset of the set of states
 module Data.Automata.DFA (
   DFA
 , mkDFA
 , walk
 , accept
 , isComplete
+, dfa2dot
 ) where
 
 import Control.Monad.State
+import Data.GraphViz
+import Data.GraphViz.Types
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Set (Set)
@@ -32,7 +41,7 @@ mkDFA locs alph trs iniLoc terms = DFA {
                                   , alphabet = Set.fromList alph
                                   , transitions = Map.fromList trs
                                   , initial = iniLoc
-                                  , terminal = Set.fromList terms
+                                  , terminals = Set.fromList terms
                                   }
 
 
@@ -50,3 +59,21 @@ accept dfa word = Set.member (evalState (walk word) dfa) (terminals dfa)
 
 isComplete :: DFA -> Bool
 isComplete dfa = all (\k -> Map.member k (transitions dfa)) [(l,a) | l <- Set.elems $locations dfa, a <- Set.elems $ alphabet dfa]
+
+
+dfa2dot :: DFA -> DotGraph String
+dfa2dot dfa = DotGraph  { strictGraph = False
+                        , directedGraph = True
+                        , graphID = Nothing
+                        , graphStatements = DotStmts    { attrStmts = [NodeAttrs [shape Circle]]
+                                                        , subGraphs = []
+                                                        , nodeStmts = (  [ DotNode "init" [shape PlainText, textLabel ""]
+                                                                        ]
+                                                                      ++ map (\n->DotNode n []) (Set.toList ((locations dfa) Set.\\ (terminals dfa)))
+                                                                      ++ map (\n->DotNode n [shape DoubleCircle]) (Set.toList (terminals dfa))
+                                                                      )
+                                                        , edgeStmts = ([DotEdge "init" (initial dfa) []]
+                                                                      ++ (map (\((n1,a),n2)->DotEdge n1 n2 [toLabel a]) $ Map.toList (transitions dfa)
+                                                                      ))
+                                                        }
+                        }
